52c9e24727cf2d8d88b80457d832fe36d100716a
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-06-03 18:03:18 -0700

    completed

--- lib/car.rb
+++ lib/car.rb
@@ -1,3 +1,16 @@
 class Car
-  #write Car class code here
+  attr_accessor :speed
+
+  def initialize
+  	@speed = 0
+  end
+
+  def accelerate(speed)
+  	@speed += speed
+  	return @speed
+  end
+
 end
+
+acura = Car.new
+bmw = Car.new
--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,22 @@
 require_relative 'car'
 
 class Race
-  #write Race class code here
+	attr_accessor :cars
+  def initialize
+  	@cars = []
+  	2.times{@cars.push(Car.new)}
+  	@cars[0].speed = rand(1..100)
+  	@cars[1].speed = rand(1..100)
+  end
+
+  def winner
+  	return @cars[0] if @cars[0].speed > @cars[1].speed
+  	return @cars[1] if @cars[1].speed > @cars[0].speed
+  end
+
+  def loser
+ 	return @cars[0] if @cars[0].speed < @cars[1].speed
+  	return @cars[1] if @cars[1].speed < @cars[0].speed
+  end
+
 end
